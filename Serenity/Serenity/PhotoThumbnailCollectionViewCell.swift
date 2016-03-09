0d7db0d9e3cd17ca5753b9cf184fc8708f549760
osi-o <osi.otugo@gmail.com>
2016-03-08 19:01:48 -0500

    add project to git

---
+++ Serenity/Serenity/PhotoThumbnailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoThumbnailCollectionViewCell.swift
+//  
+//
+//  Created by Harold  on 2/29/16.
+//
+//
+
+import UIKit
+
+class PhotoThumbnailCollectionViewCell: UICollectionViewCell {
+    
+}
